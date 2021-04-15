78115df067e62eb649096bc44a5d2d7c0ad03192
carl <karol-nowakowski@o2.pl>
2021-04-15 09:57:14 +0200

    Create styles files

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,15 +2,36 @@ import React from "react";
 
 class Notes extends React.Component {
 
-    
+
     render() {
         const { children } = this.props;
         return (
-            <>
-                <h1>notes</h1>
-                {children}
-                <h1>Opis</h1>
-            </>
+            <div>
+                <p>My notes:</p>
+
+                <div className="note">
+                    <p>Wykąpać psa</p>
+                    <div className="description">
+                        pamietaj aby wykąpać psa specjalnym szamponem
+                    </div>
+                    <button>edytuj</button>
+                    <button className="delete">usuń</button>
+                </div>
+
+                <div className="note">
+                    <p>Zrobić zakupy</p>
+                    <div className="description">
+                        kupić mleko, masło ,likier
+                    </div>
+                    <button>edytuj</button>
+                    <button className="delete">usuń</button>
+                </div>
+
+            </div>
+
+                
+                
+           
         );
     }
 }
